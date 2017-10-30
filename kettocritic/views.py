886175555e3a13a366a32b6321d37f1266fe8ba6
Scott Kwang <skwang0127@gmail.com>
2017-10-29 17:20:45 -0700

    add individual query functionality

--- kettocritic/views.py
+++ kettocritic/views.py
@@ -1,6 +1,8 @@
+import collections
 from kettocritic import models, serializers
 
 class ModelView():
+    model = None
     serializer_class = None
     select_query = None
     filters = []
@@ -14,39 +16,47 @@ class ModelView():
             result = f(query)
         return result
 
-    def _serialize_results(self, results):
-        if self.serializer_class is None:
+    def _serialize_query(self, query):
+        if self.serializer_class is None or query is None:
             return None
+        if not query:
+            return []
         
         serializer = self.serializer_class()
-        return list(map(serializer.get_serialized_model, results))
+        if isinstance(query, collections.Iterable):
+            return list(map(serializer.get_serialized_model, query))
+        else:
+            return serializer.get_serialized_model(query)
 
-    def query(self):
-        if self.select_query is None:
-            return
-        if self.serializer_class is None:
-            return
+    def query(self, model_id=None):
+        if self.model is None or self.select_query is None or self.serializer_class is None:
+            return None
         
-        filtered_results = self._filter_query(self.select_query)
-        serialized_results = self._serialize_results(filtered_results)
-        print(serialized_results)
+        filtered_query = self._filter_query(self.select_query)
+        if model_id:
+            filtered_query = filtered_query.where(self.model.id == model_id).first()
+        return self._serialize_query(filtered_query)
 
 
 class GameView(ModelView):
+    model = models.Game
     serializer_class = serializers.GameSerializer
     select_query = models.Game.select()
 
 
 class TeamView(ModelView):
+    model = models.Team
     serializer_class = serializers.TeamSerializer
     select_query = models.Team.select()
 
 
 class ReviewerView(ModelView):
+    model = models.Reviewer
     serializer_class = serializers.ReviewerSerializer
     select_query = models.Reviewer.select()
 
 
 class ReviewView(ModelView):
+    model = models.Review
     serializer_class = serializers.ReviewSerializer
     select_query = models.Review.select()
